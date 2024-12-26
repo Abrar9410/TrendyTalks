2dbbdb8242ef171e8d3d54b74ca2397793112c3b
Abrar Shahriar <abrarshahriar9410@gmail.com>
2024-12-26 08:46:06 +0600

    WishList done with functionalities

--- src/components/RecentBlogs.jsx
+++ src/components/RecentBlogs.jsx
@@ -3,6 +3,7 @@ import { useContext, useEffect, useState } from "react";
 import BlogCard from "./BlogCard";
 import { AuthContext } from "../providers/AuthProvider";
 import useAxiosSecure from "../hooks/useAxiosSecure";
+import Loading from "./Loading";
 
 
 const RecentBlogs = () => {
@@ -10,6 +11,7 @@ const RecentBlogs = () => {
     const [recentBlogs, setRecentBlogs] = useState([]);
     const [wishList, setWishList] = useState([]);
     const {user} = useContext(AuthContext);
+    const [dataLoading, setDataLoading] = useState(true);
     const axiosSecure = useAxiosSecure()
     
     useEffect(() => {
@@ -17,14 +19,23 @@ const RecentBlogs = () => {
         .then(res => {
             setRecentBlogs(res.data);
         })
+        setDataLoading(true);
         if (user) {
             axiosSecure.get(`/wishlist/${user.email}`)
-            .then(res => setWishList(res.data))
+            .then(res => {
+                setWishList(res.data);
+                setDataLoading(false);
+            })
         }
         else {
             setWishList([]);
+            setDataLoading(false);
         }
     },[user])
+
+    if (dataLoading) {
+        return <Loading></Loading>
+    }
 
     return (
         <div className="mb-12 w-11/12 mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 xl:gap-8">
--- src/pages/WishList.jsx
+++ src/pages/WishList.jsx
@@ -3,6 +3,11 @@ import useAxiosSecure from "../hooks/useAxiosSecure";
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
 import Loading from "../components/Loading";
+import Header from "../components/Header";
+import { Link } from "react-router-dom";
+import DataTable from "react-data-table-component";
+import Swal from "sweetalert2";
+import { toast } from "react-toastify";
 
 
 const WishList = () => {
@@ -13,12 +18,145 @@ const WishList = () => {
     const [dataLoading, setDataLoading] = useState(true);
 
     useEffect(() => {
+        fetchWishList();
+    }, [user])
+
+    const fetchWishList = () => {
         axiosSecure.get(`/wishlist/${user.email}`)
         .then(res => {
             setWishList(res.data);
             setDataLoading(false);
         })
-    }, [user])
+    }
+
+    const handleRemove = id => {
+        Swal.fire({
+            title: "Remove from WishList?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, Remove!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/wishlist/${id}`)
+                .then(res => {
+                    if (res.data.deletedCount > 0) {
+                        setDataLoading(true);
+                        fetchWishList();
+                        toast.success('Blog removed from your WishList', {
+                            position: "top-center",
+                            autoClose: 1500
+                        })
+                    }
+                })
+            }
+        });
+    }
+
+    const columns = [
+        {
+            name: "Title",
+            selector: (row) => row.blog.title,
+            sortable: true,
+            cell: (row) => (
+                <div className="flex items-center gap-3 text-start font-semibold text-base p-2">
+                    <img src={row.blog.thumbnail} alt="Blog_Thumbnail" className="w-12 h-12 rounded" />
+                    <span>{row.blog.title}</span>
+                </div>
+            ),
+            minWidth: "245px",
+            maxWidth: "450px",
+        },
+        {
+            name: "Category",
+            selector: (row) => row.blog.category,
+            sortable: true,
+            minWidth: "110px",
+            maxWidth: "200px",
+        },
+        {
+            name: "Description",
+            selector: (row) => row.blog.description,
+            sortable: true,
+            cell: (row) => <span>{row.blog.description.slice(0, 70)}...</span>, // Shortened description
+            minWidth: "130px",
+            maxWidth: "content"
+        },
+        {
+            name: "Comments",
+            selector: (row) => row.blog.comments,
+            sortable: true,
+            cell: (row) => (
+                <p><span>{row.blog.comments} <span>{row.blog.comments>1? "comments": "comment"}</span></span></p>
+            ),
+            minWidth: "115px",
+            maxWidth: "150px",
+        },
+        {
+            name: "Author",
+            selector: (row) => row.blog.userName,
+            sortable: true,
+            cell: (row) => (
+                <div className="flex justify-center items-center gap-2">
+                    <img src={row.blog.userPhoto} alt={row.blog.userName[0]} className="w-6 h-6 rounded-full" />
+                    <span>{row.blog.userName}</span>
+                </div>
+            ),
+            minWidth: "120px",
+            maxWidth: "250px"
+        },
+        {
+            name: "Published",
+            selector: (row) => row.blog.time,
+            sortable: true,
+            minWidth: "129px",
+            maxWidth: "150px"
+        },
+        {
+            name: "Action",
+            selector: (row) => row.blog.time,
+            sortable: false,
+            cell: (row) => (
+                <div className="flex flex-col justify-center items-center gap-2">
+                    <Link to={`/blogs/${row.blog._id}`} className="w-max py-1 px-2 rounded-lg bg-black text-cyan-400 hover:scale-105">Read More</Link>
+                    <button onClick={() => handleRemove(row._id)} className="w-max py-1 px-2 rounded-lg bg-red-500 text-black hover:scale-105">Remove</button>
+                </div>
+            ),
+            minWidth: "100px",
+            maxWidth: "120px"
+        },
+    ];
+
+    const tableStyles = {
+        table: {
+            style: {
+                tableLayout: "auto"
+            }
+        },
+        headCells: {
+            style: {
+                justifyContent: "center",
+                alignItems: "center",
+                textAlign: "center",
+                backgroundColor: "#f3f4f6",
+                fontWeight: "bold",
+                fontSize: "1.12rem",
+                border: "1px solid orange",
+                padding: "10px 0"
+            },
+        },
+        cells: {
+            style: {
+                justifyContent: "center",
+                alignItems: "center",
+                display: "flex",          // Ensure the cells use Flexbox
+                border: "1px solid orange",
+                padding: "10px 0",
+                textWrap: "wrap"
+            },
+        },
+    };
 
     if (dataLoading) {
         return <Loading></Loading>
@@ -26,7 +164,18 @@ const WishList = () => {
 
     return (
         <div>
-            WISHLIST
+            <Header
+            title="Your WishList"
+            subtitle="Check out your favourite Blogs from here.">
+            </Header>
+            <div className="w-11/12 mx-auto my-12 text-center overflow-x-auto">
+                <DataTable
+                    columns={columns}
+                    data={wishList}
+                    customStyles={tableStyles}
+                    defaultSortFieldId={1} // Optional: Set default sorting
+                />
+            </div>
         </div>
     );
 };
