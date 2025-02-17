ff919ef6133148fead047a83c264a0386312c0b7
Abrar Shahriar <abrarshahriar9410@gmail.com>
2025-02-17 10:13:27 +0600

    Logout confirmation color slightly changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,8 +17,8 @@ const Navbar = () => {
             title: "Are you sure you want to log out?",
             icon: "warning",
             showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
+            confirmButtonColor: "#d33",
+            cancelButtonColor: "#3085d6",
             confirmButtonText: "Yes, log out!"
         }).then((result) => {
             if (result.isConfirmed) {
